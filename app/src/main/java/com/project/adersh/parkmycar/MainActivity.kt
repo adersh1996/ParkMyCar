5d4bfee143bf463b36588e797ecebbac1bad3531
adersh1996 <adersh1996@gmail.com>
2024-02-01 15:26:05 +0530

    Initial commit

---
+++ app/src/main/java/com/project/adersh/parkmycar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.project.adersh.parkmycar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
